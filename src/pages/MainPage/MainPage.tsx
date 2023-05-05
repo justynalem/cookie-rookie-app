a382529fb9d73bf5634f64cc982f81ac327efa88
justynalem <lempickajustyna@gmail.com>
2023-05-05 17:14:12 +0200

    move logic to custom hook

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,133 +1,17 @@
-import { useState, KeyboardEvent, ChangeEvent, useEffect } from "react";
 import { ChipsList } from "../../components";
+import { useCookieRookie, InputNames } from "../../hooks";
 import { Button, Input } from "../../ui";
 import "./MainPage.scss";
 
-enum Meal {
-  breakfast = "breakfast",
-  dinner = "dinner",
-}
-
-enum InputNames {
-  Meal = "newMeal",
-  FridgeItems = "newItemsInFridge",
-  Kitchenware = "newKitchenware",
-  Order = 'order'
-}
-
-interface CookieRookieState {
-  meals: string[];
-  itemsInFridge: string[];
-  kitchenware: string[];
-  newMeal: string;
-  newItemsInFridge: string;
-  newKitchenware: string;
-  order: Meal | "";
-}
-
-interface CookieRookieRequestBody {
-  meals: string[];
-  itemsInFridge: string[];
-  kitchenware: string[];
-  order: Meal | "";
-}
-
-const LOCAL_STORAGE_KEY = "cookie_rookie_items";
 const REQUEST_URL = "/api/1683293701191-7758839300367";
 
 export function MainPage() {
-  const [items, setItems] = useState<CookieRookieState>({
-    meals: [],
-    itemsInFridge: [],
-    kitchenware: [],
-    newMeal: "",
-    newItemsInFridge: "",
-    newKitchenware: "",
-    order: "",
-    ...JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || "{}"),
-  });
-
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(items));
-  }, [items]);
-
-  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
-      handleAdd(event);
-    }
-  };
-
-  const handleOnChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setItems({ ...items, [name]: value });
-  };
-
-  const handleAdd = (event: KeyboardEvent<HTMLInputElement>) => {
-    const { name, value } = event.target as HTMLInputElement;
-    const trimmedValue = value?.trim();
-
-    if (!trimmedValue) {
-      return;
-    }
-
-    if (name === InputNames.Meal && !items.meals.includes(trimmedValue)) {
-      setItems(prevItems => ({
-        ...prevItems,
-        meals: [...prevItems.meals, trimmedValue],
-        newMeal: "",
-      }));
-    } else if (
-      name === InputNames.FridgeItems &&
-      !items.itemsInFridge.includes(trimmedValue)
-    ) {
-      setItems(prevItems => ({
-        ...prevItems,
-        itemsInFridge: [...prevItems.itemsInFridge, trimmedValue],
-        newItemsInFridge: "",
-      }));
-    } else if (
-      name === InputNames.Kitchenware &&
-      !items.kitchenware.includes(trimmedValue)
-    ) {
-      setItems(prevItems => ({
-        ...prevItems,
-        kitchenware: [...prevItems.kitchenware, trimmedValue],
-        newKitchenware: "",
-      }));
-    }
-  };
-
-  const handleRemove = (inputName: string) => (item: string) => {
-    if (inputName === InputNames.Meal && item) {
-      setItems(prevItems => ({
-        ...prevItems,
-        meals: prevItems.meals.filter(meal => meal !== item),
-      }));
-    }
-
-    if (inputName === InputNames.FridgeItems && item) {
-      setItems(prevItems => ({
-        ...prevItems,
-        itemsInFridge: prevItems.itemsInFridge.filter(
-          fridgeItem => fridgeItem !== item
-        ),
-      }));
-    }
-
-    if (inputName === InputNames.Kitchenware && item) {
-      setItems(prevItems => ({
-        ...prevItems,
-        kitchenware: prevItems.kitchenware.filter(
-          kitchenItem => kitchenItem !== item
-        ),
-      }));
-    }
-  };
+  const { handleKeyDown, handleOnChange, handleRemove, items } = useCookieRookie();
 
   const handleOnClick = async () => {
     const { itemsInFridge, kitchenware, meals, order } = items;
 
-    const body: CookieRookieRequestBody = {
+    const body = {
       itemsInFridge,
       kitchenware,
       meals,
@@ -171,7 +55,8 @@ export function MainPage() {
 
         <ChipsList
           items={items.itemsInFridge}
-          onDelete={handleRemove(InputNames.FridgeItems)}></ChipsList>
+          onDelete={handleRemove(InputNames.FridgeItems)}
+        />
         <Input
           value={items.newItemsInFridge}
           name={InputNames.FridgeItems}
@@ -182,8 +67,8 @@ export function MainPage() {
 
         <ChipsList
           items={items.kitchenware}
-          onDelete={handleRemove(InputNames.Kitchenware)}></ChipsList>
-
+          onDelete={handleRemove(InputNames.Kitchenware)}
+        />
         <Input
           value={items.newKitchenware}
           name={InputNames.Kitchenware}
@@ -191,6 +76,7 @@ export function MainPage() {
           onChange={handleOnChange}
           onKeyDown={handleKeyDown}
         />
+
         <h2 className='mainPage__containerText'> Let the magic happen!</h2>
         <Input
           value={items.order}
@@ -199,6 +85,7 @@ export function MainPage() {
           onChange={handleOnChange}
           onKeyDown={handleKeyDown}
         />
+
         <div className='mainPage__buttonContainer'>
           <Button onClick={handleOnClick}>Create recipe</Button>
         </div>
